8671812ea01632d10cdfab8a4ecdc98ebf715eca
Peter Neubauer <peter@neubauer.se>
2014-09-26 00:39:07 +0200

    testing lazy loading once more

--- wlm-maps.js
+++ wlm-maps.js
@@ -156,7 +156,7 @@ function setMarker(feature,latlng) {
     var monument; 
     monument=L.marker(latlng, {icon: icon});
     var content = $(popuptext);
-    content.appendChild(link)
+    content.append(link)
     monument.bindPopup(content, {minWidth: 300});
     return monument;
 }
