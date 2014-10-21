1b24e1517bb88d2845b6940e3876865b14bebe10
Peter Neubauer <peter@neubauer.se>
2014-10-21 13:07:00 +0200

    more escaping of klassnames

--- wlm-maps.js
+++ wlm-maps.js
@@ -139,7 +139,8 @@ function setMarker(feature,latlng) {
         .replace(/[<> \/-]/g,'_')
         .replace(/\)/g, '_')
         .replace(/\(/g, '_')
-        .replace(/\}/g, '_');
+        .replace(/\}/g, '_')
+        .replace(/'/g, '_');
 //    console.log(feature.properties.id, klass);
     popuptext = popuptext + '<tr><td colspan=2 style="text-align: center;font-size: 150%;"><button class="'+klass+'">Check Mapillary</button></td></tr>';
     popuptext = popuptext + '<tr><td colspan=2 style="text-align: center;font-size: 150%;"><div id="'+klass+'"></div></td></tr>';
